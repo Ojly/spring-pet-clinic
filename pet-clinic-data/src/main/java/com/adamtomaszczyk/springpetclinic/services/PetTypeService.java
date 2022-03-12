dbf4f81bab95fa2f2617bab9beac329fe0a3f4a1
ojly <ddreporter1124@yahoo.com>
2022-03-12 21:38:20 +0100

    Adding PetTypeService and PetTypeServiceMap

---
+++ pet-clinic-data/src/main/java/com/adamtomaszczyk/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.adamtomaszczyk.springpetclinic.services;
+
+import com.adamtomaszczyk.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
