d5bf0f55335e9630879e0e5f312f28766206c3d8
ojly <ddreporter1124@yahoo.com>
2022-03-16 18:28:25 +0100

    Implementing OwnerSDJpaService.

--- pet-clinic-data/src/main/java/com/adamtomaszczyk/springpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/adamtomaszczyk/springpetclinic/repositories/OwnerRepository.java
@@ -4,6 +4,9 @@ import com.adamtomaszczyk.springpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Optional<Owner> findByLastName(String name);
 }
