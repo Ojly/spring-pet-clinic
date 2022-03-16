00e3b5af7b2eb223a4d7cedd381b6c0aaea67984
ojly <ddreporter1124@yahoo.com>
2022-03-16 19:21:41 +0100

    Implementing Pet, PetType, Visit, Speciality SDJpaServices and VisitServiceMap

---
+++ pet-clinic-data/src/main/java/com/adamtomaszczyk/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.adamtomaszczyk.springpetclinic.services;
+
+import com.adamtomaszczyk.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/adamtomaszczyk/springpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,36 @@
+package com.adamtomaszczyk.springpetclinic.services.map;
+
+import com.adamtomaszczyk.springpetclinic.model.Visit;
+import com.adamtomaszczyk.springpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return null;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return null;
+    }
+
+    @Override
+    public void delete(Visit object) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
